af8fac75e440c1e3a460dca80034115487a0189f
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-10-07 15:29:00 -0300

    Movimentos possíveis de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,6 +43,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There in not piece on source position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {// Vai receber as coordenadas do Chess
